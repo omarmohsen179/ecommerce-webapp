12cd601b97840c40ae9848578948f7dadca88043
omarmohsen179 <mohsenomar350@gmail.com>
2022-05-28 17:17:07 +0200

    mmm

---
+++ src/Views/User/Products/EditProduct/EditProduct.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EditProduct() {
+  return <div>EditProdussct</div>;
+}
+
+export default EditProduct;
--- src/Views/User/Products/index.jsx
+++ src/Views/User/Products/index.jsx
@@ -1,54 +1,38 @@
-// function Products() {
-//   return <div></div>;
-// }
-
-// export default Products;
-
 import React from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import { useHistory } from "react-router";
+import MasterTable from "../../../Components/MasterTable/MasterTable";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "productName", headerName: "Product Name", width: 130 },
-  { field: "price", headerName: "Price", width: 130 },
-  { field: "quantity", headerName: "Quantity", width: 130 },
+  {
+    field: "name",
+    caption: "Name",
+  },
+  {
+    field: "price",
+    caption: "Price",
+    // alignment: "center",
+  },
+  {
+    field: "quantity",
+    caption: "Quantity",
+    // alignment: "center",
+  },
 ];
 
-const rows = [
-  { id: 1, price: "16000", productName: "ps5", quantity: "20" },
-  { id: 2, price: "16000", productName: "ps5", quantity: "20" },
-  { id: 3, price: "16000", productName: "ps5", quantity: "20" },
-  { id: 4, price: "16000", productName: "ps5", quantity: "20" },
-  { id: 5, price: "1200", productName: "ps4", quantity: "20" },
-  { id: 6, price: "1200", productName: "ps4", quantity: "20" },
-  { id: 7, price: "1000", productName: "ps3", quantity: "20" },
-  { id: 8, price: "16000", productName: "ps5", quantity: "20" },
-  { id: 9, price: "16000", productName: "ps5", quantity: "20" },
-  { id: 10, price: "16000", productName: "ps5", quantity: "20" },
-  { id: 11, price: "16000", productName: "ps5", quantity: "20" },
-  { id: 12, price: "16000", productName: "ps5", quantity: "20" },
-  { id: 13, price: "16000", productName: "ps5", quantity: "20" },
+const data = [
+  { name: "ps5", price: "16000", quantity: "20" },
+  { name: "ps4", price: "8000", quantity: "14" },
+  { name: "ps3", price: "4000", quantity: "20" },
 ];
 
 function index() {
   return (
-    <div
-      style={{
-        height: 400,
-        width: "80%",
-        margin: "auto",
-        backgroundColor: "white",
-        marginTop: "50px",
-        borderRadius: "5px",
-        boxShadow: "0 1px 9px 1px rgba(0, 0, 0, 0.28)",
-      }}
-    >
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
+    <div>
+      <MasterTable
+        dataSource={data}
+        colAttributes={columns}
+        onRowDoubleClick={(e) => useHistory.push("/")}
+        onRowRemoving={(e) => console.log(e)}
       />
     </div>
   );
